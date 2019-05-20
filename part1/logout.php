a74b81bccf88b26f4b1a1870348310b2ec7374e8
h421155377 <hyhhyhhyh@gmail.com>
2019-05-20 17:21:12 -0500

    debug logout

--- part1/logout.php
+++ part1/logout.php
@@ -1,4 +1,8 @@
 <?php
+ini_set("display_errors", 1);
+ini_set("track_errors", 1);
+ini_set("html_errors", 1);
+error_reporting(E_ALL);
 
 require_once('../components/head.php');
 require_once('../components/footer.php');
